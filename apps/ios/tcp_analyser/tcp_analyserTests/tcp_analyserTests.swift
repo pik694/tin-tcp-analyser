084cfd966139f6949fe5dd4e37be38119b6a68da
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-05-16 01:03:03 +0200

    Add iOS application

---
+++ apps/ios/tcp_analyser/tcp_analyserTests/tcp_analyserTests.swift
@@ -0,0 +1,36 @@
+//
+//  tcp_analyserTests.swift
+//  tcp_analyserTests
+//
+//  Created by Piotr Żelazko on 15.05.2018.
+//  Copyright © 2018 Piotr Żelazko. All rights reserved.
+//
+
+import XCTest
+@testable import tcp_analyser
+
+class tcp_analyserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
